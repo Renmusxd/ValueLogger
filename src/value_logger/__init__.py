4f891abbfc6eca32b750aad661da79463a13df4f
Sumner Hearth <sumnernh@gmail.com>
2025-05-14 11:40:27 -0400

    Moved logging to logging library, added description of env variables

--- src/value_logger/__init__.py
+++ src/value_logger/__init__.py
@@ -4,19 +4,41 @@ import os
 
 import numpy
 import numpy as np
+import cupy as cp
 
+import logging
+logger = logging.getLogger(__name__)
+logging.basicConfig(
+    level=os.environ.get('LOGLEVEL', 'INFO').upper()
+)
 
 class ValueLogger:
     # Singletons for each log file name
     loggers = {}
 
     def __init__(self, directory):
+        """
+        Logs values to a directory, behavior set by environment variables:
+        COMPRTOL: relative tolerance for comparison.
+        COMPATOL: absolute tolerance for comparison.
+        LOGTO: directory to write log files to.
+        COMPARE: directory read from and compare logged values to.
+        PANICONFAIL: if set, raise an exception if comparison fails.
+        FAILLOG: directory to write failed values to.
+        OVERWRITECOMP: if set, overwrite values with values from COMPARE.
+        LOGSUCC: if set, log success of comparison otherwise remain silent.
+        LOGZFILL: zfill length for logging.
+        :param directory: directory to log to or read from.
+        """
         self.directory = directory
         self.count = 0
         self.log_values_to = None
         self.compare_from = None
         self.panic = False
         self.panic_fail_log = None
+        self.rtol = float(os.environ.get("COMPRTOL", 1e-5))
+        self.atol = float(os.environ.get("COMPATOL", 1e-8))
+
         if os.environ.get("LOGTO") is not None:
             self.log_values_to = os.environ.get("LOGTO")
             os.makedirs(os.path.join(self.log_values_to, self.directory), exist_ok=True)
@@ -27,8 +49,9 @@ class ValueLogger:
         if os.environ.get("FAILLOG") is not None:
             self.panic_fail_log = os.environ.get("FAILLOG")
             os.makedirs(os.path.join(self.panic_fail_log, self.directory), exist_ok=True)
+        self.overwrite_comp = bool(os.environ.get("OVERWRITECOMP"))
         self.logsucc = bool(os.environ.get("LOGSUCC"))
-        self.zfill = 5
+        self.zfill = int(os.environ.get("LOGZFILL",5))
 
 
     @staticmethod
@@ -42,14 +65,34 @@ class ValueLogger:
         ValueLogger.loggers[directory].log(key, lambda: value)
 
     @staticmethod
-    def log_result_to_directory(directory: str, key: str, value: Callable[[], Any]):
+    def log_result_to_directory(directory: str, key: str, value: Callable[[], Any]) -> np.ndarray:
+        """
+        Logs the result of the value callable.
+        """
         if directory not in ValueLogger.loggers:
             ValueLogger.loggers[directory] = ValueLogger(directory)
-        ValueLogger.loggers[directory].log(key, value)
+        return ValueLogger.loggers[directory].log(key, value)
+
+    @staticmethod
+    def log_cupy(directory: str, key: str, value: cp.ndarray):
+        """
+        Logs a cupy array. If COMPARE and OVERWRITECOMP are set, overwrites value with data from the saved array.
+        This allows the user to substitute in known correct values during runtime.
+        """
+        if directory not in ValueLogger.loggers:
+            ValueLogger.loggers[directory] = ValueLogger(directory)
+        logger = ValueLogger.loggers[directory]
+        res = logger.log(key, lambda: value.get())
+        logger.overwrite_with_compare(value, res)
+
+    def overwrite_with_compare(self, value: cp.ndarray, comp_value: np.ndarray):
+        if self.overwrite_comp:
+            value[:] = cp.array(comp_value)
+            assert np.allclose(value.get(), comp_value), "FAILED TO OVERWRITE"
 
     def log(self, key: str, value: Callable[[], np.ndarray]):
         if self.log_values_to is None and self.compare_from is None:
-            return
+            return None
 
         base_filename = f"{key}_{str(self.count).zfill(self.zfill)}.npz"
         self.count += 1
@@ -63,7 +106,7 @@ class ValueLogger:
             comp_filename = os.path.join(self.compare_from, self.directory, base_filename)
             compare_value = numpy.load(comp_filename)['value']
 
-            are_all_close = np.allclose(value, compare_value)
+            are_all_close = np.allclose(value, compare_value, rtol=self.rtol, atol=self.atol)
             if not are_all_close:
                 if self.panic_fail_log is not None:
                     filename = os.path.join(self.panic_fail_log, self.directory, base_filename)
@@ -72,11 +115,13 @@ class ValueLogger:
                 ddif = (value - compare_value)
                 low, high, mean = ddif.min(), ddif.max(), np.absolute(ddif).mean()
                 if self.panic:
-                    raise RuntimeError(f"Comparison for {self.directory}/{key} failed: {low:.6f}\t{high:.6f}\t{mean:.6f}")
+                    raise RuntimeError(f"Comparison for {self.directory}/{key} failed: {low:.3e}\t{high:.3e}\t{mean:.3e}")
                 else:
-                    print(f"[-] Comparison for {self.directory}/{key} failed: {low:.6f}\t{high:.6f}\t{mean:.6f}", file=sys.stderr)
+                    logging.debug(f"[-] Comparison for {self.directory}/{key} failed: {low:.3e}\t{high:.3e}\t{mean:.3e}")
             elif self.logsucc:
                 ddif = (value - compare_value)
                 low, high, mean = ddif.min(), ddif.max(), np.absolute(ddif).mean()
-                print(f"[+] Comparison for {self.directory}/{key} succeeded: {low:.6f}\t{high:.6f}\t{mean:.6f}", file=sys.stderr)
+                logging.debug(f"[+] Comparison for {self.directory}/{key} succeeded: {low:.3e}\t{high:.3e}\t{mean:.3e}")
+            return compare_value
 
+        return value
